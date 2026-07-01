bf5912f2e56a1a2593dd3774161bbba7cc0130d0
Varraaa <mrafi.hiba@gmail.com>
2026-07-02 02:27:27 +0700

    Migration & Model 60%

---
+++ app/Models/Airline.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+// Untuk mengaktifkan fitur Factory (membuat data tiruan/palsu secara otomatis untuk uji coba database)
+use Illuminate\Database\Eloquent\Factories\HasFactory; 
+
+// Untuk menjadikan class ini sebagai Model Laravel (menghubungkan class PHP dengan tabel di database MySQL/SQLite)
+use Illuminate\Database\Eloquent\Model; 
+
+// Untuk mengaktifkan fitur "Recycle Bin" (menyembunyikan data saat dihapus tanpa benar-benar menghapusnya secara permanen dari database)
+use Illuminate\Database\Eloquent\SoftDeletes; 
+
+class Airline extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $fillable = [
+        'code',
+        'name',
+        'logo',
+    ];
+}
